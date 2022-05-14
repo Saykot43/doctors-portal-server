aa3dcb2903dfe8c02ba04d92319345eea289b972
Saykot43 <saykot.cse43@gmail.com>
2022-05-15 00:04:33 +0600

    post api done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
@@ -8,10 +9,36 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@cluster0.wkkgt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+        console.log('connect to database');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Doctor portal running')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Doctors portal run on port ${port}`)
 })
